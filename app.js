6456a1f679050d58432786b862b4ee5577224d71
Nemomenor <akivisivan@gmail.com>
2023-10-02 12:59:34 +0300

    commit

--- app.js
+++ app.js
@@ -7,14 +7,3 @@ tg.MainButton.setParams({
     text: 'Создать задачу',
     is_visible: true
 });
-
-
-    let input = form.querySelector('#taskName');
-    let errorMsg = form.querySelector('.error');
-    if (!input.value) {
-        tg.MainButton.hide();
-        errorMsg.style.display = 'block'; // show the error message
-    } else {
-        tg.MainButton.show();
-        errorMsg.style.display = 'none'; // hide the error message
-    }
