363e32b2a2c62d8aecdd5798965f3117c907e700
Nemomenor <akivisivan@gmail.com>
2023-10-02 12:49:15 +0300

    commit

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ tg.MainButton.setParams({
     is_visible: true
 });
 
+
+    let input = form.querySelector('#taskName');
+    let errorMsg = form.querySelector('.error');
+    if (!input.value) {
+        tg.MainButton.hide();
+        errorMsg.style.display = 'block'; // show the error message
+    } else {
+        tg.MainButton.show();
+        errorMsg.style.display = 'none'; // hide the error message
+    }
 let task_name = document.getElementById("task_name").value;
 let myCheck = document.getElementById("myCheck").value;
 let data = {
